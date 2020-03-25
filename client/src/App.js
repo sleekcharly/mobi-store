5fdf514a3d8f1e14565f7ae557db88ab33fc2756
sleekcharly <c_ukasoanya@yahoo.com>
2020-03-25 15:06:20 +0100

    client components created

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,16 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// bring in bootstrap
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//bring in font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h3>Hello from app</h3>
+    </React.Fragment>
   );
 }
 
