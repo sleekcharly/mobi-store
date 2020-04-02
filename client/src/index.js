b7f9f4a79b9295b91c02ae6ce0fb97f3fd112da9
sleekcharly <c_ukasoanya@yahoo.com>
2020-04-02 17:19:54 +0100

    Added proptypes to Product component

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { ProductProvider } from "./context";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
+  <ProductProvider>
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  </ProductProvider>,  
   document.getElementById('root')
 );
 
