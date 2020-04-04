1f1cefbf46cdf564444e81ad5594e3bd6017a4d7
sleekcharly <c_ukasoanya@yahoo.com>
2020-04-04 13:13:20 +0100

    remove items from Cart functionality

--- client/src/context.js
+++ client/src/context.js
@@ -86,7 +86,12 @@ class ProductProvider extends Component {
     }
 
     clearCart = () => {
-        console.log('cart was cleared');
+        this.setState(() => {
+            return {cart: []}
+        }, () => {
+            this.setProducts();
+            this.addTotals();
+        })
     }
 
     addTotals = () => {
